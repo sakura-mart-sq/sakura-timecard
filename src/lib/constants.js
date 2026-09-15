f6c54f2186a0220a61e8c78205e9b69f3e001188
sakura-mart-sq <dev.naoyuki@gmail.com>
2026-09-15 11:16:27 -0700

    Bump app version to 1.2.25

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "1.2.24";
+export const APP_VERSION = "1.2.25";
 export const STORE_KEY = "grocery-timecard-v1";
 export const TIME_ZONE = "America/Vancouver";
 export const DEFAULT_STORE_NAME = "Sakura Mart";
